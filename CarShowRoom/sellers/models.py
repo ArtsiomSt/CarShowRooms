9ee14f4eef2c3f7ea72d1a211d3b540bdddc4a8c
ArtsiomSt <arteomst2004@gmail.com>
2023-04-11 17:42:45 +0300

    Aded (through) table for manytomany rels(showroom+brand, dealer+car, dealer+showroom

--- CarShowRoom/sellers/models.py
+++ CarShowRoom/sellers/models.py
@@ -15,7 +15,8 @@ class CarShowRoom(models.Model):
     country = CountryField()
     address = models.TextField()
     phone_number = models.CharField(max_length=20)
-    car_brands = models.ManyToManyField(CarBrand)
+    car_brands = models.ManyToManyField(CarBrand, through='ShowroomBrand')
+    dealers = models.ManyToManyField('Dealer', through='DealerShowroom')
     price_category = models.CharField(
         max_length=10, choices=PriceCategory.choices, default=PriceCategory.MEDIUM
     )
@@ -28,7 +29,25 @@ class Dealer(models.Model):
     name = models.CharField(max_length=40)
     year_founded = models.IntegerField(default=datetime.datetime.now().year)
     money_turnover = models.DecimalField(max_digits=8, decimal_places=2)
-    car_list = models.ManyToManyField(Car)
+    car_list = models.ManyToManyField(Car, through='DealerCar')
     date_created = models.DateTimeField(default=datetime.datetime.now())
     date_modified = models.DateTimeField(default=datetime.datetime.now())
     is_active = models.BooleanField(default=True)
+
+
+class ShowroomBrand(models.Model):
+    car_showroom = models.ForeignKey(CarShowRoom, on_delete=models.CASCADE)
+    car_brand = models.ForeignKey(CarBrand, on_delete=models.CASCADE)
+
+
+class DealerCar(models.Model):
+    dealer = models.ForeignKey(Dealer, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    cars_amount = models.IntegerField(default=1)
+    cars_sold = models.IntegerField(default=0)
+
+
+class DealerShowroom(models.Model):
+    dealer = models.ForeignKey(Dealer, on_delete=models.CASCADE)
+    car_showroom = models.ForeignKey(CarShowRoom, on_delete=models.CASCADE)
+
