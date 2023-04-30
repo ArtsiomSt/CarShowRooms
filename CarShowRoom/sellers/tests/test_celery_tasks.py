3113ff9497258bf8502e632c6069e8b64a4262d4
ArtsiomSt <arteomst2004@gmail.com>
2023-04-30 21:13:04 +0300

    implemented test that checks the work of supply_cars_from_dealers_function

--- CarShowRoom/sellers/tests/test_celery_tasks.py
+++ CarShowRoom/sellers/tests/test_celery_tasks.py
@@ -1,11 +1,28 @@
 import random
+from datetime import timedelta
+from decimal import Decimal
 
 import pytest
-
+from django.db.models import F, Sum
+from django.utils import timezone
 from cars.models import CarBrand
 from core.enums.carenums import PriceCategory
-from sellers.models import DealerCar, ShowroomBrand, ShowroomCar
-from sellers.tasks import update_dealer_showroom_relations, update_showrooms_car
+from sellers.models import (
+    DealerCar,
+    ShowroomBrand,
+    ShowroomCar,
+    Balance,
+    SupplyHistory,
+    CarShowRoom,
+    Dealer,
+    Discount,
+    DiscountCar,
+)
+from sellers.tasks import (
+    update_dealer_showroom_relations,
+    update_showrooms_car,
+    supply_cars_from_dealers,
+)
 
 pytest_plugins = [
     "cars.tests.fixtures",
@@ -142,6 +159,8 @@ def test_showroom_car_dealer_relations_easy(get_showrooms, get_dealers, get_cars
     )
     for showroom in showrooms:
         update_showrooms_car(showroom)
+
+    #  This part if all cars that fit showrooms were added to their car list
     assert cars[0] in showrooms[0].car_list.all()
     assert cars[1] in showrooms[0].car_list.all()
     assert cars[2] in showrooms[0].car_list.all()
@@ -180,7 +199,9 @@ def test_showroom_car_dealer_relations_easy(get_showrooms, get_dealers, get_cars
             ),  # this one is best for car[4]
         ]
     )
-    update_dealer_showroom_relations()
+
+    update_dealer_showroom_relations()  # this function is USED in CELERY
+
     assert (
         ShowroomCar.objects.get(car_showroom=showrooms[0], car=cars[0]).dealer
         == dealers[0]
@@ -212,6 +233,116 @@ def test_showroom_car_dealer_relations_easy(get_showrooms, get_dealers, get_cars
     assert (
         ShowroomCar.objects.get(car_showroom=showrooms[1], car=cars[5]).dealer is None
     )
+    start_money_amount = Decimal(100000)
+    with open("file.txt", "w") as file:
+        pass
+    for showroom in showrooms:
+        showroom.balance.money_amount = start_money_amount
+        showroom.balance.save()
+
+    supply_cars_from_dealers()  # This function is used in CELERY
+
+    # Now we will check that all supplied cars were bought properly(every one received his money and gotten cars)
+    showrooms = CarShowRoom.objects.all()
+    for showroom in showrooms:
+        for car in showroom.car_list.all():
+            showroom_car_object = ShowroomCar.objects.get(
+                car_showroom=showroom, car=car
+            )
+            if showroom_car_object.dealer is not None:
+                supply_history = SupplyHistory.objects.get(
+                    car=car, car_showroom=showroom
+                )
+                dealer_car_object = DealerCar.objects.get(
+                    dealer=showroom_car_object.dealer, car=car
+                )
+                assert supply_history.car_price == dealer_car_object.car_price
+                assert (
+                    showroom_car_object.car_amount == 1 + supply_history.cars_amount
+                ), "1 because default car_amount is 1(will fix it soon)"
+            else:
+                assert (
+                    len(SupplyHistory.objects.filter(car=car, car_showroom=showroom))
+                    == 0
+                ), "If there is no dealer there should be no deal"
+        showrooms_supply_history = SupplyHistory.objects.filter(
+            car_showroom=showroom
+        ).aggregate(showrooms_spents=Sum(F("car_price") * F("cars_amount")))[
+            "showrooms_spents"
+        ]
+        assert (
+            showroom.balance.money_amount
+            == start_money_amount - showrooms_supply_history
+        )
+    dealers = Dealer.objects.all().order_by("pk")
+    for dealer in dealers:
+        dealers_supply_history = SupplyHistory.objects.filter(dealer=dealer).aggregate(
+            dealers_spents=Sum(F("car_price") * F("cars_amount"))
+        )["dealers_spents"]
+        assert (
+            dealer.balance.money_amount == dealers_supply_history
+        ), "Dealer didn't recieve his money"
+    SupplyHistory.objects.all().delete()
+
+    # Now we will check the ability of showroom to buy car by discount
+    # We will create a discount for dealer[1] for car[0], so it will
+    # better to buy this car from this dealer
+
+    discount = Discount.objects.create(
+        dealer=dealers[1],
+        discount_percent=75,
+        start_date=timezone.now(),
+        end_date=timezone.now() + timedelta(hours=2),
+    )  # so new cost should be 75% from 200
+    discount_car_object = DiscountCar.objects.create(car=cars[0], discount=discount)
+    target_dealer_car_object = DealerCar.objects.get(car=cars[0], dealer=dealers[1])
+    calculated_price = round(Decimal(0.25), 2) * target_dealer_car_object.car_price
+    assert discount_car_object.new_car_price == calculated_price
+    showroom = ShowroomCar.objects.get(car=cars[0]).car_showroom
+    showroom_car_object = ShowroomCar.objects.get(car_showroom=showroom, car=cars[0])
+    current_dealer_car_object = DealerCar.objects.get(
+        car=cars[0], dealer=showroom_car_object.dealer
+    )
+    start_balance = showroom.balance.money_amount
+    start_dealer0_balance = dealers[0].balance.money_amount
+    start_dealer1_balance = dealers[1].balance.money_amount
+    assert showroom_car_object.dealer == dealers[0]
+    assert current_dealer_car_object.car_price > discount_car_object.new_car_price
+
+    supply_cars_from_dealers()
+
+    showroom = ShowroomCar.objects.get(car=cars[0]).car_showroom
+    supply_history = SupplyHistory.objects.get(car=cars[0])
+    assert supply_history.dealer != showroom_car_object.dealer
+    assert supply_history.dealer == dealers[1]
+    assert supply_history.car_price == discount_car_object.new_car_price
+    new_showrooms_target_balance = (
+        start_balance - supply_history.cars_amount * discount_car_object.new_car_price
+    )
+    other_spents = (
+        SupplyHistory.objects.filter(car_showroom=showroom)
+        .exclude(car=cars[0])
+        .aggregate(not_target_car=Sum(F("car_price") * F("cars_amount")))[
+            "not_target_car"
+        ]
+    )
+    assert showroom.balance.money_amount + other_spents == new_showrooms_target_balance
+    assert (
+        len(SupplyHistory.objects.filter(car=cars[0]).exclude(dealer=dealers[1])) == 0
+    )
+    new_dealer0_balance = Balance.objects.get(pk=dealers[0].balance.pk).money_amount
+    new_dealer1_balance = Balance.objects.get(pk=dealers[1].balance.pk).money_amount
+    dealer1_profit = new_dealer1_balance - start_dealer1_balance
+    dealer0_profit = new_dealer0_balance - start_dealer0_balance
+    assert (
+        dealer0_profit
+        + current_dealer_car_object.car_price * supply_history.cars_amount
+        == start_dealer0_balance
+    ), "This test checked if dealer0 gained less profit since last car_supply action"
+    assert (
+        dealer1_profit
+        == start_dealer1_balance + supply_history.cars_amount * supply_history.car_price
+    ), "This test checked if dealer1 got more profit since last car_supply action"
 
 
 def random_amount_of_elements(array):
